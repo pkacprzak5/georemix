981cdff97dadfaa9e520727df7952b3d39e9d67e
Massny <pawel.fornagiel2@gmail.com>
2025-09-15 20:17:23 +0200

    feat: update package-lock.json, implement Minimap inside App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,29 @@
-import { MainWindow } from "./components/MainWindow";
-import { MapWindow } from "./components/OpenStreetMapWindow";
-import { GameStateProvider } from "./context/GameStateContext";
+import { GameStateProvider } from "./context/game-state/GameStateContext";
+import { Minimap } from "./features/minimap/Minimap";
+import { NavigationProvider } from "./lib/navigation-system/navigation-provider";
+import { Window, WindowContent } from "@/components/layout/window";
+import { Router } from "@/lib/navigation-system/router";
 
 function App() {
   return (
-    <GameStateProvider>
-      <MainWindow />
-      <MapWindow />
-    </GameStateProvider>
+    <NavigationProvider>
+      <GameStateProvider>
+        <Window
+          title="guesser.exe"
+          initialPosition={{
+            x: (window.innerWidth / 2) * 0.05,
+            y: (window.innerHeight / 2) * 0.05,
+          }}
+          className="h-[95vh] w-[95vw] overflow-hidden">
+          <WindowContent className="w-full h-full relative">
+            <div className="w-full h-full relative">
+              <Router />
+            </div>
+          </WindowContent>
+        </Window>
+        <Minimap />
+      </GameStateProvider>
+    </NavigationProvider>
   );
 }
 
