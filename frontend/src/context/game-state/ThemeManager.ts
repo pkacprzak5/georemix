6d228a1c013f3c9429091efb68983ba5753a1550
Massny <pawel.fornagiel2@gmail.com>
2025-09-17 19:21:04 +0200

    refactor: extract constants for theme change class and duration in ThemeManager

--- frontend/src/context/game-state/ThemeManager.ts
+++ frontend/src/context/game-state/ThemeManager.ts
@@ -1,10 +1,13 @@
+const THEME_CHANGE_CLASS = "theme-change"
+const THEME_CHANGE_DURATION = 1100;
+
 class ThemeManager {
   private readonly _document = window.document.documentElement;
   setTheme(theme: "light" | "dark") {
-    this._document.classList.add("theme-change")
+    this._document.classList.add(THEME_CHANGE_CLASS)
     setTimeout(() => {
-      this._document.classList.remove("theme-change")
-    }, 1100)
+      this._document.classList.remove(THEME_CHANGE_CLASS)
+    }, THEME_CHANGE_DURATION)
     switch (theme) {
       case "light":
         this._document.classList.remove("dark");
